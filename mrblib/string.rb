38591fbeb194825df6b449051ed45ebba6076523
Akira Yumiyama <yumiyama@iij.ad.jp>
2013-07-08 13:13:29 +0900

    cleanup string.rb (move to mrbgems/mruby-string-ext)
     - String#lstrip, #rstrip, #strip, #lstrip!, #rstrip!, #strip!

--- mrblib/string.rb
+++ mrblib/string.rb
@@ -134,45 +134,6 @@ class String
     self.replace([b, value, a].join(''))
   end
 
-  LSPACES__ = " \f\n\r\t\v"
-  def lstrip
-    a = 0
-    z = self.size - 1
-    a += 1 while LSPACES__.include?(self[a]) and a <= z
-    (z >= 0) ? self[a..z] : ""
-  end
-
-  RSPACES__ = " \f\n\r\t\v\0"
-  def rstrip
-    a = 0
-    z = self.size - 1
-    z -= 1 while RSPACES__.include?(self[z]) and a <= z
-    (z >= 0) ? self[a..z] : ""
-  end
-
-  def strip
-    a = 0
-    z = self.size - 1
-    a += 1 while LSPACES__.include?(self[a]) and a <= z
-    z -= 1 while RSPACES__.include?(self[z]) and a <= z
-    (z >= 0) ? self[a..z] : ""
-  end
-
-  def lstrip!
-    s = self.lstrip
-    (s == self) ? nil : self.replace(s)
-  end
-
-  def rstrip!
-    s = self.rstrip
-    (s == self) ? nil : self.replace(s)
-  end
-
-  def strip!
-    s = self.strip
-    (s == self) ? nil : self.replace(s)
-  end
-
   def %(args)
     sprintf(self, *args)
   end
