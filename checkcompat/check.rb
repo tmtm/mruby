946fc0d69c26e97523b1bdf1609c96fb111fab72
Tomoyuki Sahara <tsahara@iij.ad.jp>
2014-03-12 10:07:44 +0900

    remove "--depth 1".

--- checkcompat/check.rb
+++ checkcompat/check.rb
@@ -76,11 +76,11 @@ class Mrbgem
   def update
     return if @updated
     if Dir.exists? @dir
-      Dir.chdir @dir
-      system "git fetch --depth 1 origin"
-      system "git checkout master"
+      Dir.chdir @dir do
+        system "git pull"
+      end
     else
-      system "git clone --depth 1 #{@url} #{@dir}"
+      system "git clone #{@url} #{@dir}"
     end
     @updated = true
   end
