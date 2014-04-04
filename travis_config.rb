6aedf2ba9577ca55090fb158a0017bef3f7d5e5f
Akira Yumiyama <yumiyama.akira@gmail.com>
2014-04-04 11:42:28 +0900

    Update travis_config.rb

--- travis_config.rb
+++ travis_config.rb
@@ -3,7 +3,7 @@ MRuby::Build.new('debug') do |conf|
   enable_debug
 
   # include all core GEMs
-  conf.gembox 'full-core'
+  conf.gembox 'iij'
   conf.cc.flags += %w(-Werror=declaration-after-statement)
   conf.compilers.each do |c|
     c.defines += %w(MRB_GC_FIXED_ARENA)
@@ -14,7 +14,7 @@ MRuby::Build.new do |conf|
   toolchain :gcc
 
   # include all core GEMs
-  conf.gembox 'full-core'
+  conf.gembox 'iij'
   conf.cc.flags += %w(-Werror=declaration-after-statement)
   conf.compilers.each do |c|
     c.defines += %w(MRB_DEBUG MRB_GC_FIXED_ARENA)
@@ -25,7 +25,7 @@ end
 MRuby::Build.new('cxx_abi') do |conf|
   toolchain :gcc
 
-  conf.gembox 'full-core'
+  conf.gembox 'iij'
   conf.cc.flags += %w(-Werror=declaration-after-statement)
   conf.compilers.each do |c|
     c.defines += %w(MRB_DEBUG MRB_GC_FIXED_ARENA)
